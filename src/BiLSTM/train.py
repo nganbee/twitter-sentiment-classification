b930f38adeff734452f7ea7a4066534ade44bfef
nganbee <ngankimm510@gmail.com>
2025-09-24 14:29:03 +0700

    add DL model

---
+++ src/BiLSTM/train.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import os
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional, Dropout
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.callbacks import EarlyStopping
+
+base_dir = os.path.dirname(os.path.abspath(__file__)) 
+parent_dir = os.path.dirname(base_dir)
+parent_dir = os.path.dirname(parent_dir)
+data_train_path = os.path.join(parent_dir, "data", "processed", "training.csv")
+data_val_path = os.path.join(parent_dir, "data", "processed", "val.csv")
+model_path = os.path.join(parent_dir, "model")
+
+df_train = pd.read_csv(data_train_path)
+df_val = pd.read_csv(data_val_path)
+
+X_train = df_train['text']
+y_train = df_train['label']
+X_val = df_val['text']
+y_val = df_val['label']
+
+vocab_size = 10000
+embedding_dim = 64
+max_length = 50
+num_classes = 3
+
+#Tokenize and vectorize
+tokenizer = Tokenizer(num_words=vocab_size, oov_token="<OOV>")
+tokenizer.fit_on_texts(X_train)
+
+X_train_seq = tokenizer.texts_to_sequences(X_train)
+X_val_seq   = tokenizer.texts_to_sequences(X_val)
+
+X_train_seq = pad_sequences(X_train_seq, maxlen=max_length, padding='post')
+X_val_seq   = pad_sequences(X_val_seq, maxlen=max_length, padding='post')
+
+model = keras.Sequential([
+    Embedding(input_dim=vocab_size, output_dim=embedding_dim, input_length=max_length),
+    Bidirectional(LSTM(64, dropout=0.3, recurrent_dropout=0.3)),
+    Dropout(0.5), # Adding dropout to avoid overfitting
+    Dense(3, activation='softmax')
+])
+
+model.compile(
+    loss='sparse_categorical_crossentropy', # using labels = [0, 1, 2]
+    optimizer='adam', 
+    metrics=['accuracy']
+)
+
+early_stop = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+
+model.fit(
+    X_train_seq, y_train, 
+    epochs=10, 
+    batch_size=32, 
+    validation_data=(X_val_seq, y_val),
+    callbacks=[early_stop]
+)
